14ba1d2cc2b6ed2e97e8cb8fdcb3fbde41726920
CerzaJ <142043589+CerzaJ@users.noreply.github.com>
2026-05-14 21:38:40 -0600

    Added libraries to train the models

--- src/trainer_model.py
+++ src/trainer_model.py
@@ -1,2 +1,5 @@
-def train_model(): 
-	pass
+import joblib
+import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, recall_score, f1_score
